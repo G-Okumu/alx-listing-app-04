6f69466e0c795c623876eb7dc0e6ae6bee91fa77
George-Okumu <gokumu12@gmail.com>
2025-09-06 22:40:43 +0300

    setup

---
+++ pages/property/[id].tsx
@@ -0,0 +1,25 @@
+import { PROPERTYLISTINGSAMPLE } from "@/constants";
+import { useRouter } from "next/router";
+import PropertyDetail from "@/components/property/PropertyDetail";
+import BookingSection from "@/components/property/BookingSection";
+import ReviewSection from "@/components/property/ReviewSection";
+
+export default function PropertyPage(){
+    const router = useRouter();
+    const { id } = router.query;
+
+    const property = PROPERTYLISTINGSAMPLE.find((item) => item.id === Number(id));
+
+
+    if(!property) return <p>Property not found</p>;
+
+    return(
+        <div>
+            <PropertyDetail property={property}/>
+
+            <BookingSection price={property.price}/>
+
+            <ReviewSection reviews={property.reviews}/>
+        </div>
+    )
+}
